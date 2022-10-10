9b1d0e822f9c9a851123157463c3e4403696ffc2
Danika Enad <danika.enad@gmail.com>
2022-10-09 18:53:04 -0700

    restart of app

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,4 @@
-require('dotenv').config()
-
 const express = require('express')
 
-
-// express app
+//express app
 const app = express()
-
-// routes
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
-})
-
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port 4000')
-})
